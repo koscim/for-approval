cff2fd1c964d380fd03da8ba9ea71573bffb27a1
Monica Kosciuk <koscim@gmail.com>
2018-01-29 20:40:46 -0500

    Move navbar into separate NavBar.js component

---
+++ src/components/NavBar.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+class NavBar extends Component {
+  render() {
+    return(
+      <nav className="navbar navbar-dark bg-dark">
+        <a className="navbar-brand" href="#">For Approval</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse"
+          data-target="#navbarForApproval" aria-controls="navbarForApproval"
+          aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarForApproval">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Home<span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Link</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" href="#">Disabled</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="http://example.com" id="dropdown01"
+                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+              <div className="dropdown-menu" aria-labelledby="dropdown01">
+                <a className="dropdown-item" href="#">Action</a>
+                <a className="dropdown-item" href="#">Another action</a>
+                <a className="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">About</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/topics">Topics</Link>
+            </li>
+          </ul>
+          <form className="form-inline my-2 my-md-0">
+            <input className="form-control" type="text" placeholer="Search" aria-label="Search"/>
+          </form>
+        </div>
+      </nav>
+    )
+  }
+}
+
+export default NavBar;
