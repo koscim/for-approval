f4be8678cb9ed2484e16467270d7fdca0d8983db
Monica Kosciuk <koscim@gmail.com>
2018-02-06 23:46:39 -0500

    Add cypress tests for dropdown toggle

---
+++ cypress/integration/navbar_spec.js
@@ -0,0 +1,38 @@
+describe('NavBar', () => {
+  beforeEach(() => {
+    cy.seedAndVisit();
+  });
+
+  describe('when the navbar-toggler is not toggled', () => {
+    it('should not have a visible div with expanded elements', () => {
+      cy.get('.navbar').should('contain', 'For Approval');
+      cy.get('.navbar').children('button').should('be.visible');
+      cy.get('.navbar').children('div').should('not.be.visible');
+    });
+  });
+
+  describe('when the navbar-toggler is toggled', () => {
+    beforeEach(() => {
+      cy.get('.navbar').children('button').click();
+    })
+    it('should have a visible div with expanded elements', () => {
+      cy.get('.navbar').children('div').should('be.visible');
+    })
+    it('should have a dropdown element', () => {
+      cy.get('#dropdown01').should('be.visible');
+    })
+    it('should have four nav-links', () => {
+      cy.get('.nav-link').should('have.length', 4);
+    })
+  })
+
+  describe('when the dropdown-menu in navbar is clicked', () => {
+    beforeEach(() => {
+      cy.get('.navbar').children('button').click();
+      cy.get('#dropdown01').click();
+    })
+    it('should have three dropdown-items', () => {
+      cy.get('.dropdown-item').should('have.length', 3);
+    })
+  })
+})
