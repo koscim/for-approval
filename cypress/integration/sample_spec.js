98d0d7b9299741f4462ebbef0f33cacdcf6995d3
Monica Kosciuk <koscim@gmail.com>
2018-02-06 23:27:31 -0500

    test when the navbar is toggled and not toggled

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -9,12 +9,21 @@ describe('NavBar', () => {
     cy.seedAndVisit();
   });
 
-  it('cy.get() - query DOM elements', () => {
-    cy.get('.navbar').should('contain', 'For Approval');
-    cy.get('.navbar').children('button').should('be.visible');
-    cy.get('.navbar').children('div').should('not.be.visible');
-    cy.get('.navbar').children('button').click();
-    cy.get('.navbar').children('div').should('be.visible');
-    // cy.contains('Search').children('i').should('have.class', 'fa-search')
+  describe('when the navbar-toggler is not toggled', () => {
+    it('should not have a visible div with expanded elements', () => {
+      cy.get('.navbar').should('contain', 'For Approval');
+      cy.get('.navbar').children('button').should('be.visible');
+      cy.get('.navbar').children('div').should('not.be.visible');
+    });
+  });
+
+  describe('when the navbar-toggler is toggled', () => {
+    it('should have a visible div with expanded elements', () => {
+      cy.get('.navbar').should('contain', 'For Approval');
+      cy.get('.navbar').children('button').should('be.visible');
+      cy.get('.navbar').children('div').should('not.be.visible');
+      cy.get('.navbar').children('button').click();
+      cy.get('.navbar').children('div').should('be.visible');
+    })
   })
 })
