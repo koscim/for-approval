bb756fe047c55548547b30acd91fe85e19e072c2
Monica Kosciuk <koscim@gmail.com>
2018-02-06 23:24:15 -0500

    Add cypress tests for unexpanded navbar

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -3,3 +3,18 @@ describe('My First Test', () => {
     expect(true).to.equal(true);
   })
 })
+
+describe('NavBar', () => {
+  beforeEach(() => {
+    cy.seedAndVisit();
+  });
+
+  it('cy.get() - query DOM elements', () => {
+    cy.get('.navbar').should('contain', 'For Approval');
+    cy.get('.navbar').children('button').should('be.visible');
+    cy.get('.navbar').children('div').should('not.be.visible');
+    cy.get('.navbar').children('button').click();
+    cy.get('.navbar').children('div').should('be.visible');
+    // cy.contains('Search').children('i').should('have.class', 'fa-search')
+  })
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('seedAndVisit', (seedData = []) => {
+  cy.server();
+  cy.visit('http://localhost:3000')
+})
