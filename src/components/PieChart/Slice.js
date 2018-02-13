24f59896d5ba707c38d51f3ec4c80820313070b7
Monica Kosciuk <koscim@gmail.com>
2018-02-13 00:51:44 -0500

    Create Pie.js and Slice.js components for d3 pie chart

---
+++ src/components/PieChart/Slice.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { arc } from 'd3-shape';
+
+export default class Slice extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    let { value, fill, innerRadius = 0, outerRadius} = this.props;
+
+    const path = arc()
+      .outerRadius(outerRadius)
+      .innerRadius(innerRadius)
+
+    return (
+      <path d={path(value)} fill={fill}/>
+    );
+  }
+}
--- src/containers/TrackContainer.js
+++ src/containers/TrackContainer.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import Chart from '../components/Chart';
-
+import PieChart from '../components/PieChart';
+import Pie from '../components/PieChart/Pie';
 class TrackContainer extends Component {
   constructor(props) {
     super(props);
-
   }
 
   render() {
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    let minViewportSize = Math.min(width, height);
+    let radius = (minViewportSize * 0.9) / 2;
+    let x = width / 2;
+    let y = height / 2;
+    let data = [5, 2, 7, 1, 1, 3, 4, 9];
     return (
       <div>
         <div className="jumbotron">
@@ -21,6 +28,9 @@ class TrackContainer extends Component {
         </div>
         <div className="App-chart-container">
           <Chart />
+        </div>
+        <div>
+          <Pie x={500} y={500} radius={radius} data={data}/>
         </div>
       </div>
     )
